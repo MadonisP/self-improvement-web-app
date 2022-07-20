1b0b48fca5ed30be4de00aa08aae0451a9f446cb
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-07-20 22:24:12 +0300

    deleted config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,7 @@ import {getFirestore} from 'firebase/firestore'
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlPpwx1qPObjOSF0wr1s3VabxIFo7iGtA",
-  authDomain: "blogsite-2aa61.firebaseapp.com",
-  projectId: "blogsite-2aa61",
-  storageBucket: "blogsite-2aa61.appspot.com",
-  messagingSenderId: "1081946603616",
-  appId: "1:1081946603616:web:afc238b6599131468b0305"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
